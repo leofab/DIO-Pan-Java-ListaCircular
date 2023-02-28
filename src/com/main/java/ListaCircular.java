731c469e7703d9f0bda3d8418295668d37758a13
leofab <leofabr3@gmail.com>
2023-02-28 14:52:35 -0300

    method getNo() implement

--- src/com/main/java/ListaCircular.java
+++ src/com/main/java/ListaCircular.java
@@ -8,6 +8,24 @@ public class ListaCircular<T> {
 	
 	public boolean isEmpty() {
 		return this.tamanhoLista ==0 ? true : false;
+		
+	}
+	
+	private No<T> getNo(int index){
+		if(this.isEmpty())
+			throw new IndexOutOfBoundsException("A lista está vazia!");
+			
+			if(index == 0) {
+				return this.calda;
+			
+		}
+			
+			No<T> noAuxiliar = this.calda;
+			for(int i = 0; (i < index) & (noAuxiliar != null); i++) {
+				noAuxiliar = noAuxiliar.getNoProximo();
+			}
+				
+			return noAuxiliar;
 	}
 	
 	public int size() {
