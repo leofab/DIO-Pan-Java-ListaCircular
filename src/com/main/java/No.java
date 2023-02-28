3bc7401c9bf62d0e038403eb0af5c9dc8379245f
leofab <leofabr3@gmail.com>
2023-02-28 14:38:12 -0300

    first commit

---
+++ src/com/main/java/No.java
@@ -0,0 +1,31 @@
+package com.main.java;
+
+public class No<T> {
+	
+	private T conteudo;
+	private No<T> noProximo;
+	public No(T conteudo, No<T> noProximo) {
+		super();
+		this.conteudo = conteudo;
+		this.noProximo = null;
+	}
+	public T getConteudo() {
+		return conteudo;
+	}
+	public void setConteudo(T conteudo) {
+		this.conteudo = conteudo;
+	}
+	public No<T> getNoProximo() {
+		return noProximo;
+	}
+	public void setNoProximo(No<T> noProximo) {
+		this.noProximo = noProximo;
+	}
+	@Override
+	public String toString() {
+		return "No [conteudo=" + conteudo + "]";
+	}
+	
+	
+
+}
